13c3270c8cdc7162f2c92a31a6123b769c279444
Lucrix1337 <81621126+Lucrix1337@users.noreply.github.com>
2025-05-29 10:07:11 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(() => {
-        document.querySelector('.nickname').classList.add('show');
-    }, 1000);
-    
-    setTimeout(() => {
-        document.querySelector('.social-links').classList.add('show');
-    }, 2000);
-});
